b0bf992864760874301ff12aed46137abf03d41f
Adam Glymph <64643785+AGlymph@users.noreply.github.com>
2025-08-09 15:38:06 +0900

    Update sort.rb

--- sort.rb
+++ sort.rb
@@ -1,20 +1,14 @@
-def bubble_sort(array)
-    #get number of indicies
-    end_limit = array.length-1
-    #while there is at least two unsorted numbers
-    while end_limit > 1 do
-        #go throuh all unsorted numbers
-        for i in (0..end_limit-1)
-            #swap if left is greater than right
-            if  array[i] > array[i+1]
-                temp = array[i]
-                array[i] = array[i+1]
-                array[i+1] = temp
-            end
-        end
-        end_limit -=1
+def bubble_sort(array)  
+  array[...-1].each_index do |i|
+    array[...-i].each_index do |index| 
+      if array[index] > array[index + 1]
+        temp = array[index]
+        array[index] = array[index + 1]
+        array[index + 1] = temp
+      end
     end
-    array
+  end
+  array
 end
 
-p bubble_sort([4,99,78,2,0,1])
+p bubble_sort([4,3,78,2,0,2])
